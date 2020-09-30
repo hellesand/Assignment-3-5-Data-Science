a56a047b27220bfb123c4aff9be6004affb96b2b
Helle Sandsdalen <hsa056@post.uit.no>
2020-09-30 22:27:08 +0200

    Cleanup and comment

--- Assignment-3/Assignment-3-web scraping-DataCamp.py
+++ Assignment-3/Assignment-3-web scraping-DataCamp.py
@@ -1,58 +1,60 @@
-""" 
-Create a long data frame by scraping the names of all DataCamps courses in R & Python:
-
-https://www.datacamp.com/courses/tech:r
-https://learn.datacamp.com/courses/tech:python
-
-Your final data frame should have two variables, one for “tech” and one for “language”.
-"""
-
 import requests
 from bs4 import BeautifulSoup   
-import json
 import pandas as pd
 from tabulate import tabulate
 
+''' Lists for storing the course titles '''
 R_courses = []
 Python_courses = []
 
-R_url = 'https://www.datacamp.com/courses/tech:r'
-Python_url = 'https://www.datacamp.com/courses/tech:python'
+def extract_data():
+    ''' Method for scraping the web page '''
+    R_url = 'https://www.datacamp.com/courses/tech:r'
+    Python_url = 'https://www.datacamp.com/courses/tech:python'
+    page = requests.get(R_url)
+    python_page = requests.get(Python_url)
 
-page = requests.get(R_url)
-soup = BeautifulSoup(page.content, 'html.parser')
+    return page, python_page
 
-for body in soup.findAll('div',{'class':'course-block__body'}):
-    title = body.find('h4', {'class':'course-block__title'}).text
-    R_courses.append(title)
+def parse_data(page, liste):
+    ''' Method that locate course title '''
+    soup = BeautifulSoup(page.content, 'html.parser')
+    for body in soup.findAll('div',{'class':'course-block__body'}):
+        title = body.find('h4', {'class':'course-block__title'}).text
+        liste.append(title)
 
-
-python_page = requests.get(Python_url)
-python_soup = BeautifulSoup(python_page.content, 'html.parser')
-
-for body in python_soup.findAll('div',{'class':'course-block__body'}):
-    title = body.find('h4', {'class':'course-block__title'}).text
-    Python_courses.append(title)
-
-R_DataFrame = pd.DataFrame(R_courses, columns=['Tech'])
-R_DataFrame.insert(0, 'Language', 'R')
-# R_DataFrame['Language'] = 'R'
-
-Python_DataFrame = pd.DataFrame(Python_courses,columns=['Tech'])
-Python_DataFrame.insert(0, 'Language', 'Python')
-
-frames = [R_DataFrame, Python_DataFrame]
-combined = pd.concat(frames)
-
-neatTable = tabulate(combined, showindex=False, headers=combined.columns)
-
-text_file = open("CourseTable.txt", "w")
-n = text_file.write(neatTable)
-text_file.close()
-
-combined.to_csv("CourseTable.csv", sep='\t', index=False, header=True)
+def make_frame(liste, language):
+    ''' Method that creates dataframe and appends language '''
+    DataFrame = pd.DataFrame(liste, columns=['Tech'])
+    DataFrame['Language'] = language
+    return DataFrame
 
 
+def concatenate_and_store_dataframes(R_Frame, Python_Frame):
+    ''' Method that concatinates R courses and python corses and stores it in file '''
+    frames = [R_DataFrame, Python_DataFrame]
+    combined = pd.concat(frames)
+    frames = [R_DataFrame, Python_DataFrame]
+    combined = pd.concat(frames)
+
+    neatTable = tabulate(combined, showindex=False, headers=combined.columns)
+
+    text_file = open("CourseTable.txt", "w")
+    n = text_file.write(neatTable)
+    text_file.close()
+
+    combined.to_csv("CourseTable.csv", sep='\t', index=False, header=True)
+
+
+if __name__ == "__main__":
+    R_Page, Python_Page = extract_data()
+
+    parse_data(R_Page, R_courses)
+    parse_data(Python_Page, Python_courses)
+
+    R_DataFrame = make_frame(R_courses, 'R')
+    Python_DataFrame = make_frame(Python_courses, 'Python')
+    concatenate_and_store_dataframes(R_DataFrame, Python_DataFrame)
 
 
 
