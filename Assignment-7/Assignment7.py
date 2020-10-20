c9f5dfbe82246b7a9e7a6a6c2ad4ba399ec1bc2d
Helle Sandsdalen <hsa056@post.uit.no>
2020-10-20 19:31:36 +0200

    init assignment 7

---
+++ Assignment-7/Assignment7.py
@@ -0,0 +1,9 @@
+import pandas as pd
+# Download the 2017, 2018 and 2019 US Data (zip), and the User’s Guide (pdf). Merge the files.
+
+# Write code that shows the data on the following three topics. Focus on comparing 2017, 2018 and 2019 over the year (months)? Use plots.
+
+# 1) The proportion of boys to girls?
+# 2) The average birth weight in grams by gender?
+# 3) The proportion of boys to girls by day of birth.
+
